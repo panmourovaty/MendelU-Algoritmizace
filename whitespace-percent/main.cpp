c6ffe086b36344c0203201e92bccb0ba3d87fdce
Dominik Hejl <panmourovaty@centrum.cz>
2025-11-12 16:14:41 +0100

    fix whitespace-percent logic

--- whitespace-percent/main.cpp
+++ whitespace-percent/main.cpp
@@ -5,12 +5,16 @@ int main() {
     int celkem_znaku = 0;
     int bile_znaky = 0;
 
-    while (std::cin >> vstup) {
+    while (std::cin.get(vstup)) {
         celkem_znaku++;
-        if (vstup == ' ') bile_znaky++;
-        else if (vstup == '\t') bile_znaky++;
-        else if (vstup == '\n') bile_znaky++;
+        if (vstup == ' ' || vstup == '\t' || vstup == '\n') {
+            bile_znaky++;
+        }
     }
 
-    std::cout << bile_znaky * 100.0 / celkem_znaku;
+    if (celkem_znaku > 0) {
+        std::cout << bile_znaky * 100.0 / celkem_znaku;
+    }
+
+    return 0;
 }
